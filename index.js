64d2e9b0dc430abd73b71034521c21dcb5f55106
Daniel Ulloa Sandoval <daniel.97201020@gmail.com>
2021-11-25 10:30:53 -0600

    M

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const mqtt = require('mqtt');
 const mysql = require('mysql');
 const { MongoClient } = require('mongodb');
-const uri = "mongodb+srv://agroTech:itcolima6@cluster0.qmrmc.mongodb.net/agroTech?retryWrites=true&w=majority";
-const clientM = new MongoClient(uri,{useNewUrlParser: true, useUnifiedTopology:true});
-const client = mqtt.connect('mqtt://broker.mqttdashboard.com');
+
+const uri = "mongodb+srv://agroTech:itcolima6@cluster0.qmrmc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const clientM = new MongoClient(uri,{useNewUrlParser: true, useUnifiedTopology: true });
+var settings = {
+  clientId: "mqtt-control-access-server-348324098",
+  keepalive: 6000,
+  clean: false,
+  reconnectPeriod: 1000 * 1
+}
+const client = mqtt.connect('mqtt://broker.mqttdashboard.com', settings);
 function parseJSON(mensaje){
     var array = JSON.parse(mensaje);
     return array;
@@ -42,14 +49,14 @@ function resepcionDeMensaje(array, topic){
     var hum_t = array.hum_t;
     var ph = array.ph;
     var errores = array.errores;
-    if(parseInt(hum_t) < 30){
+    if(parseInt(hum_t) < 50){
         salida = 10;
       if(parseInt(hum_a) < 30 && parseInt(temp) > 30){
         salida = salida + 5;
       } else if(parseInt(hum_a) < 30 && parseInt(temp) <= 30){
         salida = salida + 3;
       } else {
-        salida = salida + 2;
+
       }
     } else {
       if(parseInt(hum_a) < 30 && parseInt(temp) > 30){
@@ -60,7 +67,32 @@ function resepcionDeMensaje(array, topic){
     }
     var topics = topic.split('/');
     var node = topics[1];
-    var today = new Date();
+    var str = "";
+    var currentTime = new Date();
+    var hours = currentTime.getHours();
+    var minutes = currentTime.getMinutes();
+    var seconds = currentTime.getSeconds();
+    var month = currentTime.getMonth()+1;
+    var year = currentTime.getFullYear();
+    var day = currentTime.getDate();
+    if (minutes < 10) {
+        minutes = "0" + minutes
+    }
+    if (seconds < 10) {
+        seconds = "0" + seconds
+    }
+    if(month < 10){
+      month = "0"+month;
+    }
+    if(day < 10){
+        day = "0"+day;
+    }
+    str += day +"-"+month+"-"+year+ "  "+hours + ":" + minutes + ":" + seconds+ " ";
+    if(hours > 11){
+        str += "PM"
+    } else {
+        str += "AM"
+    }
     let myObj =
       {
         node: node,
@@ -70,7 +102,7 @@ function resepcionDeMensaje(array, topic){
         hum_t: hum_t,
         ph: ph,
         errores: errores,
-        date: today.getTime()
+        date: str
       };
     const ready = connectDBB(myObj);
     return salida;
@@ -78,7 +110,7 @@ function resepcionDeMensaje(array, topic){
 }
 
 function EventoConectar() {
-  client.subscribe('agroTech/Nodo_1', function(err){
+  client.subscribe('NodeLime/Nodo_1', function(err){
     if(!err){
       console.log("NO HUBO ERRORES");
     }
@@ -90,9 +122,8 @@ function EventoMensaje(topic, message){
   var arrayJSON = parseJSON(message.toString());
   var respuesta = resepcionDeMensaje(arrayJSON,topic);
   if(parseInt(respuesta) > 0){
-    client.publish('agroTech/Nodo_1/valvulas', respuesta.toString());
+    client.publish('NodeLime/Nodo_1/valvulas', respuesta.toString());
   }
-  client.end();
 }
 
 client.on('connect',EventoConectar);
